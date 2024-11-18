76dd50e7e982feeb0e538ff874d06827f237fc18
dawid.chorazy <dawid.chorazy@microsoft.wsei.edu.pl>
2024-11-18 17:10:24 +0100

    init

--- WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
+++ WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31a97d6ebe7c1a887e88644baf1d328eeee3baa6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ce74bbc4a787461181c85a19cbe34d2e0c0a76a")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
